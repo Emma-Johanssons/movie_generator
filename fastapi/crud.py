052352c876a09086cbe5cb510698c00a38cbc4f7
Emma <mailtoemmajohansson@gmail.com>
2025-01-02 12:28:59 +0100

    miss-spelled word

--- fastapi/crud.py
+++ fastapi/crud.py
@@ -3,7 +3,7 @@ from passlib.context import CryptContext
 from models import User
 from schemas import UserCreate
 
-pwd_context = CryptContext(schemes=["bcrypt"], depracted="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_user_by_username(db:Session, username:str):
     return db.query(User).filter(User.username == username).first()
